183636bbc95669b9396b060be8b9d35992d61d03
Ivan Loire <(none)>
2016-10-18 21:49:53 +1100

    fix tests

--- test/test-ping-service-contains.js
+++ test/test-ping-service-contains.js
@@ -1,5 +1,5 @@
 var assert = require('assert');
-var stuPingServiceFactory = require('../index');
+var HTTPContainsPingService = require('../index');
 
 describe('ping service http-contains', function(){
 
@@ -7,7 +7,7 @@ describe('ping service http-contains', function(){
   var service;
 
   function getMockedpingServiceFactory (mockedBody){
-    return new stuPingServiceFactory({
+    return new HTTPContainsPingService({
       dependencies: {
         request: {
           get: function(options, callback){
@@ -40,28 +40,29 @@ describe('ping service http-contains', function(){
     }
   });
 
-  it('should complain if configuration is missing', function(){
+  it('should complain if configuration is missing', function(done){
     delete service.pingServiceOptions;
-    pingService = new stuPingServiceFactory();
-    assert.throws(function(){
-      pingService.ping(service, function(){});
-    })
+    pingService = new HTTPContainsPingService();
+    pingService.ping(service, function(err){
+      assert.equal(err, 'http-contains plugin configuration is missing');
+      done();
+    });
   });
 
-  it('should complain if contains configuration is missing', function(){
+  it('should complain if contains configuration is missing', function(done){
     delete service.pingServiceOptions['http-contains'].contains;
-    pingService = new stuPingServiceFactory();
-    assert.throws(function(){
-      pingService.ping(service, function(){});
-    })
+    pingService = new HTTPContainsPingService();
+    pingService.ping(service, function(err){
+      assert.ok(err);
+      assert.equal(err, 'http-contains plugin configuration is missing');
+      done();
+    });
   });
 
   it('should not complain if notContains configuration is missing', function(done){
     delete service.pingServiceOptions['http-contains'].notContains;
-    pingService = getMockedpingServiceFactory('this is it');
-    pingService.ping(service, function(){
-      done();
-    });
+    pingService = getMockedpingServiceFactory('ping');
+    pingService.ping(service, done);
   });
 
   describe('contains matcher', function(){
@@ -76,9 +77,7 @@ describe('ping service http-contains', function(){
 
     it('should succeed if body does contain term', function(done){
       pingService = getMockedpingServiceFactory('this is a ping');
-      pingService.ping(service, function(error, body, response, elapsedTime){
-        done(error);
-      });
+      pingService.ping(service, done);
     });
   });
 
@@ -94,9 +93,7 @@ describe('ping service http-contains', function(){
 
     it('should succeed if body does not contain term', function(done){
       pingService = getMockedpingServiceFactory('this is a ping');
-      pingService.ping(service, function(error, body, response, elapsedTime){
-        done(error);
-      });
+      pingService.ping(service, done);
     });
 
   });
